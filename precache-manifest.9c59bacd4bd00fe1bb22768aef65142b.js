c62d52ce56d20eb7b7ffc6c11ba68e4ba8837abd
ManjuBanu <manjubanu0823@gmail.com>
2022-03-31 01:35:37 +0530

    Updates

---
+++ precache-manifest.9c59bacd4bd00fe1bb22768aef65142b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabd901465e23ac9daee52b65ecff47a",
+    "url": "/EcomSite/index.html"
+  },
+  {
+    "revision": "fc48af71daef19129256",
+    "url": "/EcomSite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f2b89d1ec3cd3b1deed7",
+    "url": "/EcomSite/static/js/2.a4047548.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/EcomSite/static/js/2.a4047548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd177ccf20c8ff448ab8",
+    "url": "/EcomSite/static/js/3.6b108747.chunk.js"
+  },
+  {
+    "revision": "dec3584a645ed2f1a3af",
+    "url": "/EcomSite/static/js/4.c0941985.chunk.js"
+  },
+  {
+    "revision": "9c943fad67a11f743d28",
+    "url": "/EcomSite/static/js/5.1c57e0ce.chunk.js"
+  },
+  {
+    "revision": "da142767d35286c9efa6",
+    "url": "/EcomSite/static/js/6.a66c8ad1.chunk.js"
+  },
+  {
+    "revision": "2ba04601c87815e0b1f9",
+    "url": "/EcomSite/static/js/7.90f35cce.chunk.js"
+  },
+  {
+    "revision": "aef1fabe9e6ff8a7445e",
+    "url": "/EcomSite/static/js/8.0e4c4768.chunk.js"
+  },
+  {
+    "revision": "fc48af71daef19129256",
+    "url": "/EcomSite/static/js/main.1c7f27d6.chunk.js"
+  },
+  {
+    "revision": "4bcef2189197903e1979",
+    "url": "/EcomSite/static/js/runtime-main.4593d52c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/EcomSite/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/EcomSite/static/media/shopping-bag.30f07071.svg"
+  }
+]);
